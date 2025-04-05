ab60947d814f72f1b313bdec54d8be0b54387a8f
Eric Kropf <post.eric_k@live.de>
2025-04-05 12:55:58 +0200

    Added base for new ItemActions

---
+++ XUiC_LootFilterItemActionList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LootFilter
+{
+	public class XUiC_LootFilterItemActionList : XUiController
+	{
+
+		public enum LootFilterItemActionListTypes
+		{
+			None,
+			LootFilter,
+			LootFilterItemStack
+		}
+
+
+
+	}
+}
